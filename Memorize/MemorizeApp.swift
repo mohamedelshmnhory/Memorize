0c374716b588503f3922f477f643b44e2a23b8e4
mohamednasserMg <95683227+mohamednasserMg@users.noreply.github.com>
2022-01-15 13:26:11 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by mohamed nasser on 15/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
